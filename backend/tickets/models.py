6c8d3938489615c403188bbdf9b30a90489914de
Valentin Okhotin <valentin.okhotin@gmail.com>
2018-01-18 01:35:26 +0300

    version 1.2

--- backend/tickets/models.py
+++ backend/tickets/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Ticket(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
-    haveRead = models.BooleanField
+    haveRead = models.BooleanField(default=False)
--- backend/tickets/views.py
+++ backend/tickets/views.py
@@ -5,5 +5,5 @@ from .serializers import TicketSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all().order_by('-created_at')
+    queryset = Ticket.objects.filter(haveRead=False).order_by('-created_at')
     serializer_class = TicketSerializer
